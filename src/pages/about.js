31120c5e3809ef3c3f3d72cf4344e3e03e91f830
y4shiro <y4shironao@gmail.com>
2021-06-19 10:41:38 +0900

    Header コンポーネント修正

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,7 @@ export default function About() {
   return (
     <div style={{ color: "teal" }}>
       <Header headerText="About Gatsby" />
+      <Header headerText="It's pretty cool" />
       <p>Such wow. Very React.</p>
     </div>
   )
