57bbe99942fecfadb7891597b4a025172a3d1d53
y4shiro <y4shironao@gmail.com>
2021-06-19 10:50:10 +0900

    Contact コンポーネントを作成し、Link コンポーネントでルーティングする

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default function Home() {
   return (
     <div style={{ color: "purple" }}>
-      <h1>Hello Gatsby!</h1>
+      <Link to="/contact/">Contact</Link>
+      <Header headerText="Hello Gatsby!" />
       <p>What a world.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
